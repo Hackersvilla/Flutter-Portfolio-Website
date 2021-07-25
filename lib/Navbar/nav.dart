77cd643dc2f8d7556d5a156def1d0c29d53d6cda
Harvinder <Hackersvilla776@gmail.com>
2021-07-25 12:28:39 +0530

    done

--- lib/Navbar/nav.dart
+++ lib/Navbar/nav.dart
@@ -1,5 +1,6 @@
+import 'dart:js';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'responsive.dart';
 
 class nav extends StatefulWidget {
   nav({Key? key}) : super(key: key);
@@ -18,8 +19,9 @@ class _navState extends State<nav> {
         children: [
           //for logo
           logo_w(),
-          middle_w(),
-          last_w()
+          if (!isMobile(context)) middle_w(),
+          if (!isMobile(context)) last_w(),
+          if (isMobile(context)) mobile_menu(),
         ],
       ),
     );
@@ -40,28 +42,28 @@ Widget middle_w() {
     child: Row(
       children: [
         Container(
-          margin: EdgeInsets.only(right: 20),
+          margin: EdgeInsets.only(right: 25),
           child: Text(
             "Home",
             style: TextStyle(color: Colors.white),
           ),
         ),
         Container(
-          margin: EdgeInsets.only(right: 20),
+          margin: EdgeInsets.only(right: 25),
           child: Text(
             "Blogs",
             style: TextStyle(color: Colors.white),
           ),
         ),
         Container(
-          margin: EdgeInsets.only(right: 20),
+          margin: EdgeInsets.only(right: 25),
           child: Text(
             "Projects",
             style: TextStyle(color: Colors.white),
           ),
         ),
         Container(
-          margin: EdgeInsets.only(right: 20),
+          margin: EdgeInsets.only(right: 25),
           child: Text(
             "Services",
             style: TextStyle(color: Colors.white),
@@ -77,8 +79,21 @@ Widget last_w() {
       decoration: BoxDecoration(
           color: Colors.green[300], borderRadius: BorderRadius.circular(10)),
       child: Container(
-        width: 80,
+        width: 100,
         height: 40,
-        child: Center(child: Text("Contact Me")),
+        child: Center(
+            child: Text(
+          "Contact Me",
+          style: TextStyle(fontSize: 13),
+        )),
       ));
+}
+
+Widget mobile_menu() {
+  return Container(
+    child: Icon(
+      Icons.menu,
+      color: Colors.white,
+    ),
+  );
 }
