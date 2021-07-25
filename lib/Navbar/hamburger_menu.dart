621bdd3aaa9aa90d1ddf9c631322c94b2da448e0
Harvinder <Hackersvilla776@gmail.com>
2021-07-25 17:43:54 +0530

    done

--- lib/Navbar/hamburger_menu.dart
+++ lib/Navbar/hamburger_menu.dart
@@ -10,39 +10,29 @@ class HamburgerMenu extends StatefulWidget {
 class _HamburgerMenuState extends State<HamburgerMenu> {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      // Add a ListView to the drawer. This ensures the user can scroll
-      // through the options in the drawer if there isn't enough vertical
-      // space to fit everything.
-      child: ListView(
-        // Important: Remove any padding from the ListView.
-        padding: EdgeInsets.zero,
-        children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
+    return Container(
+      decoration: BoxDecoration(color: Colors.black),
+      child: Drawer(
+        elevation: 16.0,
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(child: Image.asset('assest/close.png')),
+            ListTile(
+              title: const Text('Home'),
             ),
-            child: Text('Drawer Header'),
-          ),
-          ListTile(
-            title: const Text('Item 1'),
-            onTap: () {
-              // Update the state of the app
-              // ...
-              // Then close the drawer
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: const Text('Item 2'),
-            onTap: () {
-              // Update the state of the app
-              // ...
-              // Then close the drawer
-              Navigator.pop(context);
-            },
-          ),
-        ],
+            ListTile(
+              title: const Text('Blogs'),
+            ),
+            ListTile(
+              title: const Text('Projects'),
+            ),
+            ListTile(
+              title: const Text('Services'),
+            ),
+          ],
+        ),
       ),
     );
   }
